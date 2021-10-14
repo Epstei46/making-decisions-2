36d2291eb1cdb3cf40365082d8ee7349bca9386b
Steven Epstein <sbepstein42@gmail.com>
2021-10-14 14:50:08 -0400

    Beginner Problems Done

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -6,7 +6,8 @@ var arr = [10,20,30,40,50,60]
   Create a variable named 'firstItem' and set it equal to the first value of the 'arr' array.
 */
 
-var firstItem = arr[0]
+//Code Here
+let firstItem = arr[0]
 console.log(firstItem)
 
 
@@ -16,6 +17,9 @@ console.log(firstItem)
 */
 
 //Code Here
+let lastItemRemoved = arr.pop()
+console.log(lastItemRemoved)
+console.log(arr)
 
 ////////// PROBLEM 3 //////////
 
@@ -28,7 +32,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 */
 
 //Code Here
-
+for (let i = 0; i < family.length; i++){
+  console.log(family[i])
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -43,7 +49,11 @@ let evensArr = []
 */
 
 //Code Here
-
+for (let i = 0; i < nums.length; i++){
+  if (nums[i] % 2 === 0){
+    evensArr.splice(1,0,nums[i])
+  }
+} console.log(evensArr)
 
 
 ////////// PROBLEM 5 //////////
@@ -53,11 +63,22 @@ var score = 74
 // Do not edit the code above.
 
 /*
-  Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
+  Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, 
+  console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
 
 //Code Here
-
+if (score >= 90){
+  console.log("A")
+} else if (score >= 80){
+  console.log("B")
+} else if (score >= 70){
+  console.log("C")
+} else if (score >= 60){
+  console.log("D")
+} else if (score < 60){
+  console.log("F")
+}
 
 ////////// Intermediate Problems //////////
 
